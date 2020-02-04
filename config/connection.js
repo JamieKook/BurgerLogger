677386c158be773eece87f201a08681fa39bfeb1
Jamie <kookjc6@gmail.com>
2020-02-03 22:34:09 -0600

    added JawsDB as mysql host

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,19 @@
 const {MySQL} = require("mysql-promisify"); 
 
-const db= new MySQL({
-    host: "localhost", 
-    port: 3306,
-    user: "jamie",
-    password: "1234pass",
-    database: "burger_logger_db"
-}); 
+
+if (process.env.JAWSDB_URL){
+    const db = new MySQL(
+     process.env.JAWSDB_URL
+    )
+} else {
+    const db= new MySQL({
+        host: "localhost", 
+        port: 3306,
+        user: "jamie",
+        password: "1234pass",
+        database: "burger_logger_db"
+    }); 
+}
+
 
 module.exports = db; 
