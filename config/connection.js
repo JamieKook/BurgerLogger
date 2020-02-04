c33387426be7b12c3950f6b21cbbac0c73602e88
Jamie <kookjc6@gmail.com>
2020-02-03 22:47:53 -0600

    changed db from const to let to redefine in if statement

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,11 @@
 const {MySQL} = require("mysql-promisify"); 
-
-
+let db = null; 
 if (process.env.JAWSDB_URL){
-    const db = new MySQL(
+     db = new MySQL(
      process.env.JAWSDB_URL
     )
 } else {
-    const db= new MySQL({
+     db= new MySQL({
         host: "localhost", 
         port: 3306,
         user: "jamie",
